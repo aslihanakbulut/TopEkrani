477bf58873f47697d95db1878ba82a9d046d41b5
Aslıhan AKBULUT <56192462+aslihanakbulut@users.noreply.github.com>
2022-02-09 00:07:39 +0300

    Add files via upload

---
+++ TopEkranı/TopEkranı/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TopEkranı
+{
+    public partial class Form1 : Form
+    {
+        int ekrandaki_top_sayisi = 0;
+        List<RoundedButton> toplar = new List<RoundedButton>();
+
+
+        Color[] renkler = {Color.Blue,Color.Red,Color.Purple,Color.Brown,Color.DimGray };
+
+
+        int sayac = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void yeni_top_ekle()
+        {
+            Random random = new Random();
+            int konum_x = random.Next(100, this.Width-100);
+            int konum_y = random.Next(100, this.Height-100);
+
+            int aci = random.Next(0, 360);
+
+            int renk_id = ekrandaki_top_sayisi; //sirayla farkli renkte top eklenmesi saglandi
+            RoundedButton yeniTop = new RoundedButton(renkler[renk_id], konum_x, konum_y, (float)aci);
+            toplar.Add(yeniTop);
+            ekrandaki_top_sayisi++;
+            this.Controls.Add(toplar.ElementAt(ekrandaki_top_sayisi-1));
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            yeni_top_ekle();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            if(sayac>10*10) // her 10 saniyede bir top ekleme saglandi
+            {
+                sayac = 0;
+                if(ekrandaki_top_sayisi<5) // ekrandaki top sayisi 5 olana kadar yeni top eklenir
+                {
+                   yeni_top_ekle();
+                }
+                
+            }
+            foreach (var top in toplar)
+            {
+                top.hareketlendir(this.Height, this.Width);
+            }
+        }
+    }
+}
---
+++ TopEkranı/TopEkranı/RoundedButton.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.ComponentModel;
+
+namespace TopEkranı
+{
+
+    class RoundedButton : Button
+    {
+        private int borderSize = 0;
+        private int borderRadius = 30;
+        private float hareket_acisi = 0;
+        private Color borderColor = Color.Transparent;
+
+        public int BorderSize
+        {
+            get { return borderSize; }
+            set
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+
+        public int BorderRadius
+        {
+            get { return borderRadius; }
+            set
+            {
+                borderRadius = value;
+                this.Invalidate();
+            }
+        }
+
+        public Color BorderColor
+        {
+            get { return borderColor; }
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+
+
+        public Color BackgroundColor
+        {
+            get { return this.BackColor; }
+            set { this.BackColor = value; }
+        }
+        public RoundedButton(Color renk,int x, int y, float hareket_acisi)
+        {
+
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.Size = new Size(60, 60);
+            this.BackColor = renk;
+            this.ForeColor = Color.White;
+            this.Resize += new EventHandler(Button_Resize);
+            this.Text = "";
+
+            this.Location = new Point(x, y);
+            this.hareket_acisi = hareket_acisi;
+            this.Show();
+        }
+        private GraphicsPath GetFigurePath(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            float curveSize = radius * 2F;
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+
+
+            Rectangle rectSurface = this.ClientRectangle;
+            Rectangle rectBorder = Rectangle.Inflate(rectSurface, -borderSize, -borderSize);
+            int smoothSize = 2;
+            if (borderSize > 0)
+                smoothSize = borderSize;
+
+            if (borderRadius > 2)
+            {
+                using (GraphicsPath pathSurface = GetFigurePath(rectSurface, borderRadius))
+                using (GraphicsPath pathBorder = GetFigurePath(rectBorder, borderRadius - borderSize))
+                using (Pen penSurface = new Pen(this.Parent.BackColor, smoothSize))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                {
+                    pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+                    this.Region = new Region(pathSurface);
+
+                    pevent.Graphics.DrawPath(penSurface, pathSurface);
+
+
+                    if (borderSize >= 1)
+
+                        pevent.Graphics.DrawPath(penBorder, pathBorder);
+                }
+            }
+
+        }
+
+
+        private void Button_Resize(object sender, EventArgs e)
+        {
+            if (borderRadius > this.Height)
+                borderRadius = this.Height;
+        }
+
+
+        public void hareketlendir(int form_height, int form_width)
+        {
+            float hareket_hizi = 10;
+            int yeni_x =  (int)(hareket_hizi * Math.Cos(hareket_acisi / 180 * 3.14)) + this.Location.X ;
+            int yeni_y = (int)(hareket_hizi * Math.Sin(hareket_acisi / 180 * 3.14)) + this.Location.Y;
+
+
+            if(yeni_y<1) //yukarı
+            {
+                hareket_acisi = hareket_acisi*-1;
+                if(hareket_acisi<0)
+                {
+                    hareket_acisi += 360;
+                }
+            }
+            if(yeni_y>form_height-120)  //asagi
+            {
+                hareket_acisi = hareket_acisi*-1;
+                if (hareket_acisi < 0)
+                {
+                    hareket_acisi += 360;
+                }
+            }
+            
+            
+            //sağ ve soldan gecis
+            if(yeni_x<-60 && Math.Abs(hareket_acisi)>90)
+            {
+                yeni_x = form_width - 60;
+            }
+            if(yeni_x> form_width && Math.Abs(hareket_acisi) < 90)
+            {
+                yeni_x = -60;
+            }
+
+            this.Location = new Point(yeni_x, yeni_y);
+        }
+
+    }
+
+}
